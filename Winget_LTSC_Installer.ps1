8dd34dd791eef21eed9c799a61aafb052cbdafbe
Sasa Dermanovic <45603793+SasaDermanovic@users.noreply.github.com>
2024-03-25 22:24:52 +0100

    Update Winget_LTSC_Installer.ps1
    - Code refactor
    - Updated script to fetch and install latest release of Winget as of March 2024

--- Winget_LTSC_Installer.ps1
+++ Winget_LTSC_Installer.ps1
@@ -1,10 +1,74 @@
 Write-Host "Attempting to download and install dependencies"
-Add-AppxPackage "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
-Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.0" -OutFile "microsoft.ui.xaml.2.7.0.zip"
-Expand-Archive .\microsoft.ui.xaml.2.7.0.zip
-Add-AppPackage .\microsoft.ui.xaml.2.7.0\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx
 
-Write-Host "Dependencies installed, attempting to download and install Winget"
-Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.5.2201/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.5.2201/e9f07317387242099dd02cb57aa04c37_License1.xml -OutFile .\e9f07317387242099dd02cb57aa04c37_License1.xml
-Add-AppxProvisionedPackage -Online -PackagePath .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -LicensePath .\e9f07317387242099dd02cb57aa04c37_License1.xml -Verbose
+# Download and install VCLibs package
+try 
+{
+  Add-AppxPackage "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -Verbose
+}
+catch
+{
+  Write-Host "Failed to install VCLibs package. Error: $_" -ForegroundColor Red
+  exit 1
+}
+
+# Download XAML package
+try 
+{
+  Invoke-WebRequest -Uri "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.8.6" -OutFile "microsoft.ui.xaml.2.8.6.zip" -ErrorAction Stop
+}
+catch
+{
+  Write-Host "Failed to download XAML package. Error: $_" -ForegroundColor Red
+  exit 1
+}
+
+# Extract XAML package
+try 
+{
+  Expand-Archive .\microsoft.ui.xaml.2.8.6.zip -Force -ErrorAction Stop
+} 
+catch
+{
+  Write-Host "Failed to extract XAML package. Error: $_" -ForegroundColor Red
+  exit 1
+}
+
+# Install XAML package
+try
+{
+  Add-AppPackage .\microsoft.ui.xaml.2.8.6\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.8.appx -Verbose
+}
+catch 
+{
+  Write-Host "Failed to install XAML package. Error: $_" -ForegroundColor Red
+  exit 1
+}
+
+Write-Host "Dependencies installed successfully."
+
+Write-Host "Attempting to download and install Winget"
+
+# Download Winget package
+try 
+{
+  Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.7.10661/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile ".\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -ErrorAction Stop
+  Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.7.10661/9ea36fa38dd3449c94cc839961888850_License1.xml" -OutFile ".\9ea36fa38dd3449c94cc839961888850_License1.xml" -ErrorAction Stop
+} 
+catch 
+{
+  Write-Host "Failed to download Winget package. Error: $_" -ForegroundColor Red
+  exit 1
+}
+
+# Install Winget package
+try 
+{
+  Add-AppxProvisionedPackage -Online -PackagePath ".\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -LicensePath ".\9ea36fa38dd3449c94cc839961888850_License1.xml" -Verbose
+} 
+catch 
+{
+  Write-Host "Failed to install Winget package. Error: $_" -ForegroundColor Red
+  exit 1
+}
+
+Write-Host "Winget installed successfully."
